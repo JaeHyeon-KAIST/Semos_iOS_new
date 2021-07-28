dcc05ca305f1f052237f7e44438bc2cdacccd35c
JaeHyeon <alvinlog@kaist.ac.kr>
2021-07-28 21:12:41 +0900

    1.0.1

--- Semos/AppDelegate.swift
+++ Semos/AppDelegate.swift
@@ -13,7 +13,6 @@ import UserNotifications
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    var myVariable: String = "Hello Swift"
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
@@ -36,7 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        print("\(#function)")
     }
 }
 
@@ -45,79 +43,5 @@ extension AppDelegate: MessagingDelegate {
         print("Firebase registration token: \(fcmToken!)")
         let dataDict:[String: String] = ["token": fcmToken!]
         NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
-        myVariable = "\(fcmToken!)"
     }
 }
-
-
-
-//import UIKit
-//import Firebase
-//import FirebaseMessaging
-//import FirebaseAnalytics
-//
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//  func application(
-//    _ application: UIApplication,
-//    didFinishLaunchingWithOptions
-//      launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-//  ) -> Bool {
-//
-//    // 1
-//    FirebaseApp.configure()
-//    // 2
-//    FirebaseConfiguration.shared.setLoggerLevel(.min)
-//
-//    // 1
-//    UNUserNotificationCenter.current().delegate = self
-//    // 2
-//    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-//    UNUserNotificationCenter.current().requestAuthorization(
-//      options: authOptions) { _, _ in }
-//    // 3
-//    application.registerForRemoteNotifications()
-//
-//    Messaging.messaging().delegate = self
-//
-//    return true
-//  }
-//}
-//
-//extension AppDelegate: UNUserNotificationCenterDelegate {
-//  func userNotificationCenter(
-//    _ center: UNUserNotificationCenter,
-//    willPresent notification: UNNotification,
-//    withCompletionHandler completionHandler:
-//    @escaping (UNNotificationPresentationOptions) -> Void
-//  ) {
-//    completionHandler([[.banner, .sound]])
-//  }
-//
-//  func userNotificationCenter(
-//    _ center: UNUserNotificationCenter,
-//    didReceive response: UNNotificationResponse,
-//    withCompletionHandler completionHandler: @escaping () -> Void
-//  ) {
-//    completionHandler()
-//  }
-//}
-//
-//func application(
-//  _ application: UIApplication,
-//  didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-//) {
-//  Messaging.messaging().apnsToken = deviceToken
-//}
-//
-//extension AppDelegate: MessagingDelegate {
-//  func messaging(
-//    _ messaging: Messaging,
-//    didReceiveRegistrationToken fcmToken: String?
-//  ) {
-//    let tokenDict = ["token": fcmToken ?? ""]
-//    NotificationCenter.default.post(
-//      name: Notification.Name("FCMToken"),
-//      object: nil,
-//      userInfo: tokenDict)
-//  }
-//}
